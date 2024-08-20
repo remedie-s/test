82031fa7420de42bfe73747d8891dc1ef6d35e1d
remedie-s <tree1808@gmail.com>
2024-08-20 15:53:57 +0900

    Feat: 게시판-페이징 기능1
    엔티티, 레포지토리, 서비스, 컨트롤라, 간단한 템플릿 구동
    단, SQL에서 내부 오류 발생

---
+++ src/main/java/com/web/demo/repository/NaverReviewsRepository.java
@@ -0,0 +1,15 @@
+package com.web.demo.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.web.demo.entity.NaverReviews;
+
+
+public interface NaverReviewsRepository extends JpaRepository<NaverReviews, Long> {
+	// 페이징(Page, Pageable) 형태의 리뷰 데이터를 가져오는 메소드 선언
+	// findALL() => 모든 데이터를 가져온다
+	// 아래처럼 선언하면 => 특정 위치에서 특정 개수만큼 페이징하여 필요한 것만 가져온다.
+	Page<NaverReviews> findAll(Pageable pageable);
+}
