0c0e80661abac51f9b95c069e90371ebee2ea145
remedie-s <tree1808@gmail.com>
2024-08-22 14:32:53 +0900

    Feat: 0822오후2시반

--- src/main/java/com/web/demo/controller/FileApiController.java
+++ src/main/java/com/web/demo/controller/FileApiController.java
@@ -11,8 +11,15 @@ package com.web.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.web.demo.dto.MemberDto;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RequestMapping("/file")
 @Controller
@@ -22,13 +29,25 @@ public class FileApiController {
 	public String upload() {
 		return "file_process"; // file_process.html 생성
 	}
-//	// 파일 업로드 실제 처리 제공
-//	// 일반 데이터 + 파일 동시 전송
-//	@PatchMapping("/upload")
-//	public String upload() {
-//		return "";
-//	}
-//	// 파일 다운로드 실제 처리 제공
+	
+	// 파일 업로드 실제 처리 제공
+	// 일반 데이터 + 파일 동시 전송
+	@PostMapping("/upload")
+	@ResponseBody
+	public String upload( MemberDto member,// 나이, 이름, 주소 정보를 통으로 받아서 전달
+			@RequestParam("uploadFile") MultipartFile uploadFile, // 파일을 받아서 전달
+			HttpServletRequest req) {
+		// 1. 파일 저장할 결로 획득 (여기서는 톰캣 내부, 실제로는 클라우드의 스토리지 선택)
+		String path = req.getServletContext().getRealPath("");// 저장할 위치(서버측)
+		System.out.println("path:"+path);
+		String filename = uploadFile.getOriginalFilename();// 파일명
+		// 2. 저장
+		// 3. 결과 응답
+		
+		
+		return "응답";
+	}
+	// 파일 다운로드 실제 처리 제공
 //	@RequestMapping("/upload")
 //	public String download() {
 //		return "";
---
+++ src/main/java/com/web/demo/dto/MemberDto.java
@@ -0,0 +1,31 @@
+package com.web.demo.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 사용자 정보를 FormData 형식으로 통으로 보냈다
+ * 사용자 정보를 통으로 받아줄 형식이 필요 => DTO로 받겠다.
+ */
+@Getter
+@Setter
+@ToString
+public class MemberDto {
+	
+	private int age;
+	private String name;
+	private String addr ;
+	@Builder
+	public MemberDto(int age, String name, String addr) {
+		super();
+		this.age = age;
+		this.name = name;
+		this.addr = addr;
+	}
+	
+	
+	
+
+}
